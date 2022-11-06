2498289a06d50a0ae8d1bc2a5b90edad1ec68caa
Hoxton <za546321@yahoo.com.tw>
2022-11-06 18:02:24 +0800

    312

--- src/main/java/com/example/imguploader/ImgUploaderApplication.java
+++ src/main/java/com/example/imguploader/ImgUploaderApplication.java
@@ -1,9 +1,12 @@
 package com.example.imguploader;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +17,7 @@ import java.nio.Buffer;
 @SpringBootApplication
 public class ImgUploaderApplication extends Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ImgUploaderApplication.class, args);
         Application.launch(args);
@@ -21,12 +25,14 @@ public class ImgUploaderApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/s.fxml"));
+//        System.out.println("root = " + root);
         BorderPane borderPane = new BorderPane();
-        Button button = new Button("我是按鈕");
-        borderPane.setBottom(button);
+        Pane root    = FXMLLoader.load(getClass().getResource("/fxml/teet.fxml"));
+//        Button button = new Button("我是按鈕");
+//        borderPane.setBottom(button);
 //        borderPane.getChildren().add(borderPane);
-        Scene scene = new Scene(borderPane);
-
+        Scene scene = new Scene(root,400,200);
         stage.setScene(scene);
         stage.show();
     }
