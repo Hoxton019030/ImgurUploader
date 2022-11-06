9d2859b71b64ac998c0401476e84898760b766f5
Hoxton <za546321@yahoo.com.tw>
2022-11-06 14:41:13 +0800

    set variable

--- src/main/java/com/example/imguploader/ImgUploaderApplication.java
+++ src/main/java/com/example/imguploader/ImgUploaderApplication.java
@@ -1,19 +1,33 @@
 package com.example.imguploader;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.*;
+import java.nio.Buffer;
 
 @SpringBootApplication
 public class ImgUploaderApplication extends Application {
 
     public static void main(String[] args) {
         SpringApplication.run(ImgUploaderApplication.class, args);
+        Application.launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        BorderPane borderPane = new BorderPane();
+        Button button = new Button("我是按鈕");
+        borderPane.setBottom(button);
+//        borderPane.getChildren().add(borderPane);
+        Scene scene = new Scene(borderPane);
 
+        stage.setScene(scene);
+        stage.show();
     }
 }
